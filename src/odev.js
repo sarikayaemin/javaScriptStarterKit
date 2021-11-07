9bcf93f69347d5532f4e73d7d003081ca1463978
sarikayaemin <sarikayaemin64@gmail.com>
2021-11-07 23:49:31 +0300

    odev1

---
+++ src/odev.js
@@ -0,0 +1,15 @@
+//1000'e kadarki tüm mükemmel sayıları listeleyen programı yazınız.
+
+for (let i=1; i<= 1000; i++) {
+    let total = 0
+
+    for (let k = 1; k <= i ; k ++) {
+        if (i % k == 0) {
+            total = total + j 
+        }
+    }
+
+    if (i * 2 == total) {
+        console.log("its perfect numbers: " + i)
+    }
+} 
